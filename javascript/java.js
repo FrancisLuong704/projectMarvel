1ec3531ff61806be90a1493645f4cbb3da03a242
YoungFrank <fluong95@gmail.com>
2018-09-28 13:23:57 -0400

    fixed for loop and url

--- javascript/java.js
+++ javascript/java.js
@@ -7,7 +7,7 @@ $("#submit").on('click', function () {
     //if statement saying if it is in marvel character database than run this function below } else alert saying it is not a character
     var name = $(this).attr('data-name');
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + charInput + "+marvel&api_key=N58pplWVQxSxzn6cv54929BIxQdzCdgJ&limit=1"
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + charInput + "+marvel+movie+comic&api_key=N58pplWVQxSxzn6cv54929BIxQdzCdgJ&limit=30";
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -15,19 +15,21 @@ $("#submit").on('click', function () {
         .then(function (response) {
             var results = response.data;
             console.log(response);
-            for (var i = 0; i < characters.length; i++) {
+            var myFunc = function () {
                 var a = $('<a>');
                 var gifImage = $('<img>');
                 gifImage.addClass("gif");
-                gifImage.attr('src', results[i].images.fixed_height.url);
+                var x = Math.floor(Math.random() * 31)
+                gifImage.attr('src', results[x].images.fixed_height.url);
                 a.append(gifImage);
                 $('#gif').append(a);
             }
-        })
-        $('#character-input').val('');
+            myFunc();
+            $('#character-input').val('');
+        });
 });
 
-$('#clear').on('click', function() {
+$('#clear').on('click', function () {
     $('.gif').remove();
 })
 // click function forgif {
